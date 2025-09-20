30bcefaba191874686db4b58da6a1b2062423b89
ManavTri <manavtrivedy@gmail.com>
2025-09-20 14:22:27 -0400

    Update Star.js

--- src/game/Star.js
+++ src/game/Star.js
@@ -2,9 +2,9 @@ class Star {
     /**
      * Creates a new star object at position `pos`, with size `size`, moving in `direction`.
      * 
-     * @param {p5.Vector} pos Starting position of the star
-     * @param {number} size Size of the star (diameter)
-     * @param {p5.Vector} direction Direction vector the star moves in
+     * @param {p5.Vector} pos Starting position of the star (default: random position within canvas)
+     * @param {number} size Size of the star (diameter) (default: random between 1 and 5)
+     * @param {p5.Vector} direction Direction vector the star moves in (default: left)
      */
     constructor(pos, size = random(1, 5), direction = createVector(-1, 0)) {
         this.pos = pos || createVector(random(size, windowWidth - size), random(size, windowHeight - size));
@@ -32,22 +32,28 @@ class Star {
      * Check if star is off the screen.
      * 
      * @returns {boolean} True if the star is off the screen, false otherwise.
+     * 
+     * @param {number} width Width of the canvas (default: windowWidth)
+     * @param {number} height Height of the canvas (default: windowHeight)
      */
-    isOffScreen() {
-        return this.pos.x < -this.size || this.pos.x > windowWidth + this.size ||
-               this.pos.y < -this.size || this.pos.y > windowHeight + this.size;
+    isOffScreen(width = windowWidth, height = windowHeight) {
+        return this.pos.x < -this.size || this.pos.x > width + this.size ||
+               this.pos.y < -this.size || this.pos.y > height + this.size;
     }
 
     /**
      * Wraps the star around to the opposite side of the screen at a random height if it goes off-screen.
+     * 
+     * @param {number} width Width of the canvas (default: windowWidth)
+     * @param {number} height Height of the canvas (default: windowHeight)
      */
-    wrap() {
+    wrap(width = windowWidth, height = windowHeight) {
         if (this.pos.x < -this.size) {
-            this.pos.x = windowWidth + this.size;
-            this.pos.y = random(this.size, windowHeight - this.size);
-        } else if (this.pos.x > windowWidth + this.size) {
+            this.pos.x = width + this.size;
+            this.pos.y = random(this.size, height - this.size);
+        } else if (this.pos.x > width + this.size) {
             this.pos.x = -this.size;
-            this.pos.y = random(this.size, windowHeight - this.size);
+            this.pos.y = random(this.size, height - this.size);
         }
     }
 }
