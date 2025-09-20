26c1896f47c236b4f74c728e7c8755cff5bee20e
ManavTri <manavtrivedy@gmail.com>
2025-09-20 13:33:50 -0400

    Update sketch.js

--- src/sketch.js
+++ src/sketch.js
@@ -2,6 +2,7 @@ let stars = [];
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
+  noCursor();
   frameRate(60);
   for (let i = 0; i < 1000; i++) {
     stars.push(new Star());
