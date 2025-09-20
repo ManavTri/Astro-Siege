52810ca96839c71e7f567844d5f90fca8bffde73
Colton Rizzo <156487914+rizzocolton@users.noreply.github.com>
2025-09-20 15:21:13 -0400

    Update Ship.js

--- src/game/Ship.js
+++ src/game/Ship.js
@@ -1,16 +1,73 @@
 class Ship{
-    //class properties
-    pos=[0,0];
-    vel=[0,0];
-    acc=[0,0];
-    mass=0;
-
-    constructor(x,y,vx,vy,mass){
-        pos[0]=x;
-        pos[1]=y;
-        vel[0]=vx;
-        vel[1]=vy;
-        this.mass=mass;
+    constructor(x,y,vx,vy){
+        this.pos=createVector(x,y);
+        this.vel=createVector(vx,vy);
+        this.height=70;
+        this.width=40;
+        this.acc=createVector(0,0);
     }
 
+    move(){
+        //convert deltaTime for current frame to seconds
+        let dt=deltaTime/1000;
+        //make dv a small fraction of the velocity vector for euler integration
+        let dv=createVector(this.vel.x*dt,this.vel.y*dt);
+        //euler integration add position to dv every frame
+        this.pos.add(dv);
+        if(this.pos.x>windowWidth){
+            this.pos.x=0;
+        }
+    }
+
+    steerLeft(){
+        this.vel.rotate(-Math.PI/25);
+    }
+
+    steerRight(){
+        this.vel.rotate(Math.PI/25);
+    }
+
+    render(){
+        let angle=atan2(this.vel.y,this.vel.x);
+        //CONTROL SHIP FROM TIP
+        /*
+        let p1=createVector(-this.height,-this.width/2);
+        let p2=createVector(-this.height,this.width/2);
+        p1.rotate(angle);
+        p2.rotate(angle);
+        triangle(
+            this.pos.x,this.pos.y,
+            this.pos.x+p1.x,this.pos.y+p1.y,
+            this.pos.x+p2.x,this.pos.y+p2.y
+        );
+        */
+       //CONTROL SHIP FROM BACK
+       /*
+       let tip=createVector(this.height,0);
+       let left=createVector(0,-this.width/2);
+       let right=createVector(0,this.width/2);
+       tip.rotate(angle);
+       left.rotate(angle);
+       right.rotate(angle);
+       triangle(
+        this.pos.x+tip.x,this.pos.y+tip.y,
+        this.pos.x+left.x,this.pos.y+left.y,
+        this.pos.x+right.x,this.pos.y+right.y
+       );
+    */
+      //CONTROL SHIP FROM MIDDLE OF HEIGHT
+      
+      let tip=createVector(this.height/2,0);
+      let left=createVector(-this.height/2,-this.width/2);
+      let right=createVector(-this.height/2,this.width/2);
+      tip.rotate(angle);
+      left.rotate(angle);
+      right.rotate(angle);
+      triangle(
+       this.pos.x+tip.x,this.pos.y+tip.y,
+       this.pos.x+left.x,this.pos.y+left.y,
+       this.pos.x+right.x,this.pos.y+right.y
+      );
+      
+    }
 }
