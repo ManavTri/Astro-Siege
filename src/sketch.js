d01f3bd31484a9b6b997988e9a3cec0c1549052a
ManavTri <manavtrivedy@gmail.com>
2025-09-20 15:02:44 -0400

    Update sketch.js

--- src/sketch.js
+++ src/sketch.js
@@ -4,16 +4,25 @@ let width, height;
 // Declare new StarField object
 let starField;
 
+// Declare new PlanetPair object
+let planetPair;
+
 // Setup function to initialize the canvas and StarField
 function setup() {
+  // Set canvas dimensions
   width = windowWidth;
   height = windowHeight;
   createCanvas(width, height);
+  
   frameRate(60); // Set frame rate to 60 FPS
   background(0); // Set background to black
+  noCursor(); // Hide the cursor
 
   // Create a new StarField with 1000 stars
   starField = new StarField(1000, width, height);
+
+  // Create a new PlanetPair
+  planetPair = new PlanetPair(100, color(0, 255, 0), color(0, 0, 255), width, height);
 
   // Initial render of the star field
   starField.render();
@@ -26,4 +35,7 @@ function draw() {
 
   // Update and render the star field
   starField.render();
+
+  // Render the planet pair
+  planetPair.render();
 }
