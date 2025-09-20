ca603cc2d4d9164648fbbfe46b58e53d78e7081c
ManavTri <manavtrivedy@gmail.com>
2025-09-20 14:23:08 -0400

    Create StarField.js

---
+++ src/game/StarField.js
@@ -0,0 +1,40 @@
+class StarField {
+    /**
+     * Constructs a StarField with a specified number of stars.
+     * 
+     * @param {number} numStars - The number of stars in the star field.
+     * @param {number} width - The width of the canvas.
+     * @param {number} height - The height of the canvas.
+     */
+    constructor(numStars = 1000, width = windowWidth, height = windowHeight) {
+        this.stars = [];
+        for (let i = 0; i < numStars; i++) {
+            this.stars.push(new Star());
+        }
+        this.width = width;
+        this.height = height;
+    }
+
+    /**
+     * Updates the position of all stars and wraps them if they go off-screen.
+     */
+    update() {
+        for (let star of this.stars) {
+            star.update();
+            if (star.isOffScreen(this.width, this.height)) {
+                star.wrap(this.width, this.height);
+            }
+        }
+
+        this.render();
+    }
+
+    /**
+     * Renders all stars in the star field.
+     */
+    render() {
+        for (let star of this.stars) {
+            star.render();
+        }
+    }
+}
