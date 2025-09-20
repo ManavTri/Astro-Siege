ef743f6540d909619ff9b4c414542b7e63d1a840
Colton Rizzo <rizzocolton@gmail.com>
2025-09-20 16:42:24 -0400

    removed redundant collision function

--- src/game/Planet.js
+++ src/game/Planet.js
@@ -34,24 +34,4 @@ class Planet {
         this.side *= -1;
     }
 
-    /**
-     * Handle collision detection with a triangle defined by points p1, p2, and p3.
-     * 
-     * @returns {boolean} True if the planet collides with a triangle, false otherwise.
-     * 
-     * @param {p5.Vector} p1 First vertex of the triangle
-     * @param {p5.Vector} p2 Second vertex of the triangle
-     * @param {p5.Vector} p3 Third vertex of the triangle
-     */
-    playerCollision(p1, p2, p3) {
-        let d1, d2, d3;
-        d1 = dist(this.width / 2 + this.side * (this.width / 2 - this.size * 0.15), this.height / 2, p1.x, p1.y);
-        d2 = dist(this. width / 2 + this.side * (this.width / 2 - this.size * 0.15), this.height / 2, p2.x, p2.y);
-        d3 = dist(this.width / 2 + this.side * (this.width / 2 - this.size * 0.15), this.height / 2, p3.x, p3.y);
-        if (d1 < this.size / 2 || d2 < this.size / 2 || d3 < this.size / 2) {
-            console.log("Collision detected with planet!");
-            return true;
-        }
-        return false;
-    }
 }
