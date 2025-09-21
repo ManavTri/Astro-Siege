34a3897c1f5d057771a59c389320e631bdc05ded
Colton Rizzo <rizzocolton@gmail.com>
2025-09-20 22:54:33 -0400

    toolbar update :)

--- src/game/Toolbar.js
+++ src/game/Toolbar.js
@@ -1,11 +1,12 @@
 class Toolbar {
     constructor(){
         this.windowHeight = windowHeight;
-        this.height=50;
+        this.windowWidth = windowWidth;
+        this.height=100;
         this.buttons=[
-            {type:"blackhole",x:50,y:this.windowHeight-75,w:100,h:30,label:"Black Hole"},
-            {type:"asteroid",x:200,y:this.windowHeight-75,w:100,h:30,label:"Asteroid"},
-            {type:"clear",x:350,y:this.windowHeight-75,w:100,h:30,label:"Clear Obstacles"}
+            {type:"blackhole",x:windowWidth/3,y:this.windowHeight-75,w:100,h:30,label:"Black Hole"},
+            {type:"asteroid",x:windowWidth/2,y:this.windowHeight-75,w:100,h:30,label:"Asteroid"},
+            {type:"clear",x:windowWidth*2/3,y:this.windowHeight-75,w:100,h:30,label:"Clear Obstacles"}
         ];
         this.selected="clear";
     }
@@ -14,18 +15,23 @@ class Toolbar {
         fill(200);
         rect(0,windowHeight-this.height,width,windowHeight);
         for(let button of this.buttons){
-            fill(255);
-            rect(button.x,button.y,button.w,button.h);
+            if(this.selected!=='clear' && this.selected===button.type){
+                fill('yellow');
+            }
+            else{
+                fill(255);
+            }
+            rect(button.x - button.w/2, button.y - button.h/2, button.w, button.h);
             fill(0);
             textAlign(CENTER,CENTER);
-            text(button.label,button.x+button.w/2,button.y+button.h/2);
+            text(button.label,button.x,button.y);
         }
         pop();
     }
 
     checkClick(mx,my){
         for(let button of this.buttons){
-            if(mx>button.x && mx<button.x+button.w && my>button.y && my<button.y+button.h){
+            if(mx>button.x-button.w/2 && mx<button.x+button.w/2 && my>button.y - button.h/2 && my<button.y+button.h/2){
                 return button.type;
             }
         }
